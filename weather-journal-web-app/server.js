43eba3048621f242c9be2acce2fd544c219b99e3
Karim Mostafa <Kmohamed84@gmail.com>
2021-04-08 22:06:33 +0200

    commit

--- weather-journal-web-app/server.js
+++ weather-journal-web-app/server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+/** Listen Port */
+const port = 8000;
 
-// Setup Server
+// Setup Server 'node server.js'
+app.listen(port, () => {
+    console.log(`Server Running On: http://localhost:${port}`);
+});
+
+
+// GET Route Server Side - return project data
+app.get('/all', (request, response) => {
+    response.send(projectData);
+});
+
+//POST Route Server Side - adds data to project data
+app.post('/add', (request, response) => {
+ 
+    projectData={
+        temp:request.body.temp,
+        date:request.body.date,
+        userFeelings:request.body.userFeelings,
+        name:request.body.name
+    };
+
+    response.send(projectData);
+});
--- weather-journal-web-app/website/app.js
+++ weather-journal-web-app/website/app.js
@@ -1,5 +1,109 @@
-/* Global Variables */
+/*asynchronous web app that uses Web API and user data 
+to dynamically update the UI for a Weather-Journal App.*/
+/*submitted by Karim Mohamed*/
 
-// Create a new date instance dynamically with JS
+/* Global Variables */
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&APPID=9196db5bba9552fbc38d76ae3740ea04";
+
+//For temperature in Fahrenheit
+//const unitParam = "&units=imperial";       
+//For temperature in Celsius
+const unitParam = "&units=metric";      
+
+// create new date instance
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (d.getMonth()+1)+ "." + d.getDate() + "." + d.getFullYear();
+
+const zipCodeElement = document.getElementById('zip');
+const feelingsCodeElement = document.getElementById('feelings');
+const dateElement = document.getElementById('date');
+const tempElement = document.getElementById('temp');
+const contentElement = document.getElementById('content');
+
+
+// Event listener to get data from client
+document.getElementById('generate').addEventListener('click', generateData);
+
+// Post Data To API
+async function generateData() {
+    
+    let data = {
+        zipCode: zipCodeElement.value,
+        userFeelings: feelingsCodeElement.value,
+        date: newDate
+    };
+
+    //Post Data To Api For Get Zip Code Information
+    if(!data.zipCode){
+        alert("Hi, Please enter Zip Code")
+        return
+    }
+    getZipCodeInfo(data.zipCode)
+    .then(zipInfo => {
+        // console.log(zipInfo);
+        if (zipInfo.cod === 200){
+            data.temp = zipInfo.main.temp;    //Get Temperature
+            data.name = zipInfo.name;         //Get City Name
+        } else {
+            //Return And Show Alert If error was Found
+            return alert(zipInfo.message)
+        }
+        
+        //Now Post Data To Server side
+        postData(data);
+    })
+
+};
+
+/** Get Zip Code Information From Api */
+async function getZipCodeInfo(zipCode) {
+    try{
+    const url = `${baseURL}${zipCode}${apiKey}${unitParam}`;
+    const response = await fetch(url);
+    
+    return await response.json();
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+/** Post Data To Server side */
+async function postData(data) {
+    let response = await fetch('/add', {
+        method: 'POST',
+        credentials:"same-origin",
+        headers: { "Content-Type":"application/json",
+        Accept:"application/json",
+     },
+        body: JSON.stringify(data),
+    });
+    try {
+      
+        response.json().then(data => {
+            
+                updateUI();//Update UI Now
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/** Update UI */
+async function updateUI() {
+    let response = await fetch('/all');
+    
+    try {
+        response.json().then(data => {
+            dateElement.innerHTML = `Date is: ${data.date}`;
+            tempElement.innerHTML = `${data.name} Temp is: ${data.temp}`;
+            contentElement.innerHTML = `My Feelings is: ${data.userFeelings}`;
+            
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
